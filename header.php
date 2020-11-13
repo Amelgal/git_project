2f6a42d96614fa22836889cb6847f14994bfc074
Eugene Shalko <eugene.shalko.00@gmail.com>
2020-11-13 14:40:40 +0200

    Issue #4
    -create file header.php
    -changed index.php

---
+++ header.php
@@ -0,0 +1,12 @@
+<!Doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen"
+</head>
+<body>
+
+<hr/>
--- index.php
+++ index.php
@@ -1,13 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen"
-</head>
-<body>
+<?php include 'header.php'?>
 <div class="container">
     <div class="navbar">
         <ul class="navbar-nav nav">
