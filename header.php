1784e2163072572f9f8f3ef619f45f978cebb09c
Eugene Shalko <eugene.shalko.00@gmail.com>
2020-11-20 15:30:41 +0200

    Issue#3
    -add feature nab bar
    -change index.php

--- header.php
+++ header.php
@@ -1,12 +1,33 @@
-<!Doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen"
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-
-<hr/>
+<header class="header">
+    <div class="header_top">
+        <img src="img/lines.png" alt="" >
+    </div>
+    <div class="header_content">
+        <div class="container">
+            <div class="header_content-inner">
+                <div class="header_logo">
+                    <a href="#">
+                        <img src="img/logo.png" alt="">
+                    </a>
+                </div>
+                <nav class="menu">
+                    <ul>
+                        <li><a class="header_home" href="#"> Home </a></li>
+                        <li><a class="header_Abo" href="#"> About </a></li>
+                        <li><a class="header_Par" href="#"> Partners </a></li>
+                        <li><a class="header_Con" href="#"> Contact </a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php include 'header.php'?>
-<div class="container">
-    <div class="navbar">
-        <ul class="navbar-nav nav">
-            <li><a>Instructables</a></li>
-            <li><a>About</a></li>
-            <li><a>Sign-up</a></li>
-            <li><a>Blog</a></li>
-            <li><a>Contact Us</a></li>
-        </ul>
-    </div>
-</div>
+
 
 <div>
     Тут будет галерея
