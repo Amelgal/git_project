aed732546bcebf292979940b4bb92f1d36ba5870
Eugene Shalko <eugene.shalko.00@gmail.com>
2020-11-20 15:58:20 +0200

    Issue
    -change header

--- header.php
+++ header.php
@@ -8,17 +8,9 @@
 </head>
 <body>
 <header class="header">
-    <div class="header_top">
-        <img src="img/lines.png" alt="" >
-    </div>
     <div class="header_content">
         <div class="container">
             <div class="header_content-inner">
-                <div class="header_logo">
-                    <a href="#">
-                        <img src="img/logo.png" alt="">
-                    </a>
-                </div>
                 <nav class="menu">
                     <ul>
                         <li><a class="header_home" href="#"> Home </a></li>
