f8df7d4508bcd2b6a0601ca873af8ebfbf9c543a
Eugene Shalko <eugene.shalko.00@gmail.com>
2020-11-21 15:14:55 +0200

    Issue#12
    -added footer menu feature
    -change style.css

--- footer.php
+++ footer.php
@@ -1,5 +1,107 @@
 </div>
 <div class="clean"></div>
 </div>
+<!--<hr>-->
+<!--<div class="navbar">-->
+<!--    <ul>-->
+<!--        <li><a href="#"> Home </a></li>-->
+<!--        <li><a href="#"> About </a></li>-->
+<!--        <li><a href="#"> Partners </a></li>-->
+<!--        <li><a href="#"> Contact </a></li>-->
+<!--    </ul>-->
+<!--    <ul>-->
+<!--        <li>Home</li>-->
+<!--        <li>About</li>-->
+<!--        <li>Partners</li>-->
+<!--        <li>Contact</li>-->
+<!--    </ul>-->
+<!--</div>-->
+<footer class="wds-global-footer">
+    <div class="wds-global-footer__main">
+        <div class="wds-global-footer__column">
+            <section class="wds-global-footer__section wds-is-fandom-overview">
+                <h3 class="wds-global-footer__section-header">Explore properties</h3>
+                <ul class="wds-global-footer__links-list">
+                    <li class="wds-global-footer__links-list-item">
+                        <a href="*"
+                           class="wds-global-footer__link"
+                           data-tracking-label="explore.fandom">
+                            Fandom</a>
+                    </li>
+                    <li class="wds-global-footer__links-list-item">
+                        <a href="*/"
+                           class="wds-global-footer__link"
+                           data-tracking-label="explore.cortexrpg">
+                            Cortex RPG</a>
+                    </li>
+                    <li class="wds-global-footer__links-list-item">
+                        <a href="*/"
+                           class="wds-global-footer__link"
+                           data-tracking-label="explore.muthead">
+                            Muthead</a>
+                    </li>
+                    <li class="wds-global-footer__links-list-item">
+                        <a href="*/"
+                           class="wds-global-footer__link"
+                           data-tracking-label="explore.futhead">
+                            Futhead</a>
+                    </li>
+                </ul>
+            </section>
+        </div>
+        <div class="wds-global-footer__column">
+            <section class="wds-global-footer__section wds-is-site-overview">
+                <h3 class="wds-global-footer__section-header">Overview</h3>
+                <ul class="wds-global-footer__links-list">
+                    <li class="wds-global-footer__links-list-item">
+                        <a href="*"
+                           class="wds-global-footer__link"
+                           data-tracking-label="company-overview.about">
+                            About</a>
+                    </li>
+                    <li class="wds-global-footer__links-list-item">
+                        <a href="*"
+                           class="wds-global-footer__link"
+                           data-tracking-label="company-overview.careers">
+                            Careers</a>
+                    </li>
+                    <li class="wds-global-footer__links-list-item">
+                        <a href="*"
+                           class="wds-global-footer__link"
+                           data-tracking-label="company-overview.press">
+                            Press</a>
+                    </li>
+                    <li class="wds-global-footer__links-list-item">
+                        <a href="*"
+                           class="wds-global-footer__link"
+                           data-tracking-label="company-overview.contact">
+                            Contact</a>
+                    </li>
+                </ul>
+            </section>
+        </div>
+        <div class="wds-global-footer__column">
+            <section class="wds-global-footer__section wds-is-advertise">
+                <h3 class="wds-global-footer__section-header">Advertise</h3>
+                <ul class="wds-global-footer__links-list">
+                    <li class="wds-global-footer__links-list-item">
+                        <a href="*"
+                           class="wds-global-footer__link"
+                           data-tracking-label="advertise.media-kit">
+                            Media Kit</a>
+                    </li>
+                    <li class="wds-global-footer__links-list-item">
+                        <a href="*"
+                           class="wds-global-footer__link"
+                           data-tracking-label="advertise.contact">
+                            Contact</a>
+                    </li>
+                </ul>
+            </section>
+        </div>
+    </div>
+
+</footer>
+
 </body>
 </html>
